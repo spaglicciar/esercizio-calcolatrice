15f60c79989e81c517bfd50f835ab6c33c7d5198
Roberto Spagliccia <roberto.spagliccia@gmail.com>
2019-10-04 12:01:11 +0200

    First commit of sourcecode

---
+++ ConsoleApp4/MyClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNamespace
+{
+    public static class MyClass
+    {
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static string Sum(string a, int b)
+        {
+            return a + b;
+        }
+    }
+}
